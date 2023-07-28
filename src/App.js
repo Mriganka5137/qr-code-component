2df4b16b42ceb57bb951d65da57dd8d80038f64e
Mriganka Gogoi <gogoimriganka09@gmail.com>
2023-07-28 13:10:59 +0530

    Completed qr challenge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import qrcode from "./image-qr-code.png";
+
 function App() {
-  return <div className="card">Hello</div>;
+  return (
+    <article className="card">
+      <img src={qrcode} alt="qr code" />
+      <div className="card-details">
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </article>
+  );
 }
 
 export default App;
